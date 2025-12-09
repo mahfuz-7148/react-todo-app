562bfd603d1c7fdd9023264b9d906819b6d56103
shanto <mrahman7148@gmail.com>
2025-12-09 12:16:49 +0600

    up

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,14 @@ import {TaskBoard} from './components/taskBoard.jsx';
 
 export const App = () => {
   return (
-    <div className='bg-gradient-to-br from-slate-50 via-blue-50 to-indigo-50'>
-      <Header />
-
-      <div className="px-4 py-8">
-        <div className="flex justify-center">
-          <HeroSection />
-        </div>
-
-        <TaskBoard />
+    <>
+      <div className='bg-gradient-to-br from-slate-50 via-blue-50 to-indigo-50'>
+        <Header/>
+        <HeroSection/>
+        <TaskBoard/>
       </div>
 
-      <Footer />
-    </div>
+      <Footer/>
+    </>
   )
 }
